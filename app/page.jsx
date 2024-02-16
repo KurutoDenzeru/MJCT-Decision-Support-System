bcd90765fd1ff1ddbc208d754b50fcbc6974749e
Kuruto Denzeru <kurtdenzel51@gmail.com>
2024-02-16 22:16:04 +0800

    refactor: removed test heading

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,16 @@
-'use client';
+"use client";
 
-import React from 'react';
-import './globals.css';
+import React from "react";
+import "./globals.css";
 
-import Hero from '../components/Hero';
-import Content from '../components/Content';
+import Hero from "../components/Hero";
+import Content from "../components/Content";
 
 export default function Index() {
   return (
     <>
       <Hero />
       <hr />
-      <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
       <Content />
     </>
   );

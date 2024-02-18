002d8706171afabcc4d43ad8c57bcc7d45ad0cf9
Kuruto Denzeru <kurtdenzel51@gmail.com>
2024-02-18 23:45:35 +0800

    refactor: fix centered logo

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import Logo from './Logo';
+import Logo from "./Logo";
 
 const Hero = () => (
   <div className="hero my-5 text-center" data-testid="hero">
-    <Logo testId="hero-logo" />
+    <Logo testId="hero-logo" className="mx-auto" />
     <h1 className="mb-4" data-testid="hero-title">
       Next.js Sample Project
     </h1>
 
     <p className="lead" data-testid="hero-lead">
-      This is a sample application that demonstrates an authentication flow for a Regular Web App, using{' '}
-      <a href="https://nextjs.org">Next.js</a>
+      This is a sample application that demonstrates an authentication flow for
+      a Regular Web App, using <a href="https://nextjs.org">Next.js</a>
     </p>
   </div>
 );

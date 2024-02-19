a7d7fe810df342e746675f3bd6197c656f45ae81
Kuruto Denzeru <kurtdenzel51@gmail.com>
2024-02-20 00:46:21 +0800

    build: refactored next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+};
+
 module.exports = {
+  nextConfig,
   poweredByHeader: false,
   reactStrictMode: true,
   env: {

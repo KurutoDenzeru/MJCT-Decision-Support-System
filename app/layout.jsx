8a4c9ac14ce0577b75d81ddca316d7bc49a8c5ab
Kuruto Denzeru <kurtdenzel51@gmail.com>
2024-02-25 02:26:55 +0800

    refactor: updated metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,29 +1,38 @@
-'use client';
+"use client";
 
-import '../app/globals.css';
-import NavBar from '../components/NavBar';
-import { Container } from 'reactstrap';
-import { Metadata } from 'next'
+import "./globals.css";
+import NavBar from "../components/NavBar";
+import { Container } from "reactstrap";
+import { Metadata } from "next";
 
-import Footer from '../components/Footer';
-import React from 'react';
-import { UserProvider } from '@auth0/nextjs-auth0/client';
+import Footer from "../components/Footer";
+import React from "react";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <title>MJCT Decision Support System</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
         <link
           rel="stylesheet"
           href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
           crossOrigin="anonymous"
         />
-        <link rel="stylesheet" href="https://cdn.auth0.com/js/auth0-samples-theme/1.0/css/auth0-theme.min.css" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.auth0.com/js/auth0-samples-theme/1.0/css/auth0-theme.min.css"
+        />
       </head>
       <body>
         <UserProvider>
-          <main id="app" className="d-flex flex-column h-100" data-testid="layout">
+          <main
+            id="app"
+            className="d-flex flex-column h-100"
+            data-testid="layout"
+          >
             <NavBar />
             <Container className="flex-grow-1 mt-5">{children}</Container>
             <Footer />

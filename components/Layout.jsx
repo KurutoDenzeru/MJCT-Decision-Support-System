a260dd9f8441264bf07c29f1d53169d995cea08d
Kuruto Denzeru <kurtdenzel51@gmail.com>
2024-03-17 23:23:53 +0800

    test: push phase

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,6 +4,7 @@ import Head from 'next/head';
 
 import NavBar from './NavBar';
 import Footer from './Footer';
+import HeroFlex from './Landing/hero';
 
 const Layout = ({ children }) => (
   <>
@@ -15,11 +16,13 @@ const Layout = ({ children }) => (
         crossOrigin="anonymous"
       />
       <link rel="stylesheet" href="https://cdn.auth0.com/js/auth0-samples-theme/1.0/css/auth0-theme.min.css" />
-      <title>Next.js Sample App</title>
+      <title>MJCT Management & Services</title>
     </Head>
     <main id="app" className="d-flex flex-column h-100" data-testid="layout">
       <NavBar />
+      
       <Container className="flex-grow-1 mt-5">{children}</Container>
+      <HeroFlex />
       <Footer />
     </main>
   </>
